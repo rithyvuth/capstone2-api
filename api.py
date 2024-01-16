f6e7576e9564baaf98d84d5d3a4c2d3a809c0508
Tha Rithyvuth <tharithyvuth7@gmail.com>
2024-01-16 14:19:04 +0700

    first Initial commit

---
+++ api.py
@@ -0,0 +1,22 @@
+import json
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+text = open('text.txt', 'r', encoding='UTF-8').read()
+
+def from_txt(text_file):
+    text = open(text_file, 'r', encoding='UTF-8').read()
+    text = text.replace('\n', ' ')
+    text = text.replace(' ', '')
+    tests = text.split('។')
+    return tests
+
+
+@app.route('/get_text', methods=['GET'])
+def get_text():
+ return jsonify({'text': from_txt('khmerText.txt')})
+
+
+if __name__ == '__main__':
+   app.run()
